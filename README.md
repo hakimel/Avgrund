3aa65a8a898ca8ff684ef705bbc0de86081d6bdc
Hakim El Hattab <hakim.elhattab@gmail.com>
2012-08-18 01:14:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Avgrund
+
+A modal concept which aims to give a sense of depth between the page and modal layers.
+
+[Check out the demo page](http://lab.hakim.se/avgrund/).
+
+## History
+
+#### 0.1
+- Initial release
+
+## License
+
+MIT licensed
+
+Copyright (C) 2012 Hakim El Hattab, http://hakim.se
---
+++ js/avgrund.js
@@ -0,0 +1,72 @@
+/*!
+ * avgrund 0.1
+ * http://lab.hakim.se/avgrund
+ * MIT licensed
+ *
+ * Created by Hakim El Hattab, http://hakim.se
+ */
+(function(){
+
+	var container = document.documentElement,
+		popup = document.querySelector( '.avgrund-popup' ),
+		cover = document.querySelector( '.avgrund-cover' ),
+		currentState = null;
+
+	container.className = container.className.replace( /\s+$/gi, '' ) + ' avgrund-ready';
+
+	// Deactivate on ESC
+	function onDocumentKeyUp( event ) {
+		if( event.keyCode === 27 ) {
+			deactivate();
+		}
+	}
+
+	// Deactivate on click outside
+	function onDocumentClick( event ) {
+		if( event.target === cover ) {
+			deactivate();
+		}
+	}
+
+	function activate( state ) {
+		document.addEventListener( 'keyup', onDocumentKeyUp, false );
+		document.addEventListener( 'click', onDocumentClick, false );
+
+		removeClass( popup, currentState );
+		addClass( popup, 'no-transition' );
+		addClass( popup, state );
+
+		setTimeout( function() {
+			removeClass( popup, 'no-transition' );
+			addClass( container, 'avgrund-active' );
+		}, 0 );
+
+		currentState = state;
+	}
+
+	function deactivate() {
+		document.removeEventListener( 'keyup', onDocumentKeyUp, false );
+		document.removeEventListener( 'click', onDocumentClick, false );
+
+		removeClass( container, 'avgrund-active' );
+	}
+
+	function disableBlur() {
+		addClass( document.documentElement, 'no-blur' );
+	}
+
+	function addClass( element, name ) {
+		element.className = element.className.replace( /\s+$/gi, '' ) + ' ' + name;
+	}
+
+	function removeClass( element, name ) {
+		element.className = element.className.replace( name, '' );
+	}
+
+	window.avgrund = {
+		activate: activate,
+		deactivate: deactivate,
+		disableBlur: disableBlur
+	}
+
+})();
