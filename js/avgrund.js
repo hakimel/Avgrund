8264c14449f3535cd277465457d842f392120a03
Hakim El Hattab <hakim.elhattab@gmail.com>
2017-06-13 14:39:13 +0200

    code formatting

--- js/avgrund.js
+++ js/avgrund.js
@@ -8,72 +8,90 @@
 var Avgrund = (function(){
 
 	var container = document.documentElement,
-		popup = document.querySelector('.avgrund-popup-animate'),
-		cover = document.querySelector('.avgrund-cover'),
+		popup = document.querySelector( '.avgrund-popup-animate' ),
+		cover = document.querySelector( '.avgrund-cover' ),
 		currentState = null;
-	container.classList.add('avgrund-ready');
+
+	container.classList.add( 'avgrund-ready' );
 
 	// Deactivate on ESC
-	function onDocumentKeyUp(event) {
-		if(event.keyCode === 27) {
+	function onDocumentKeyUp( event ) {
+
+		if( event.keyCode === 27 ) {
 			deactivate();
 		}
+
 	}
 
 	// Deactivate on click outside
-	function onDocumentClick(event) {
-		if(event.target === cover) {
+	function onDocumentClick( event ) {
+
+		if( event.target === cover ) {
 			deactivate();
 		}
+
 	}
 
-	function activate(state) {
-		document.addEventListener('keyup', onDocumentKeyUp, false);
-		document.addEventListener('click', onDocumentClick, false);
-		document.addEventListener('touchstart', onDocumentClick, false);
+	function activate( state ) {
 
-		popup.classList.remove(currentState);
-		popup.classList.add('no-transition');
+		document.addEventListener( 'keyup', onDocumentKeyUp, false );
+		document.addEventListener( 'click', onDocumentClick, false );
+		document.addEventListener( 'touchstart', onDocumentClick, false );
+
+		popup.classList.remove( currentState );
+		popup.classList.add( 'no-transition' );
 		if(state)
-			popup.classList.add(state);
+			popup.classList.add( state );
 
-		setTimeout(function() {
-			popup.classList.remove('no-transition');
-			container.classList.add('avgrund-active');
-		}, 0);
+		setTimeout( function() {
+			popup.classList.remove( 'no-transition' );
+			container.classList.add( 'avgrund-active' );
+		}, 0 );
 
 		currentState = state;
+
 	}
 
 	function deactivate() {
-		document.removeEventListener('keyup', onDocumentKeyUp, false);
-		document.removeEventListener('click', onDocumentClick, false);
-		document.removeEventListener('touchstart', onDocumentClick, false);
 
-		container.classList.remove('avgrund-active');
-		popup.classList.remove('avgrund-popup-animate');
+		document.removeEventListener( 'keyup', onDocumentKeyUp, false );
+		document.removeEventListener( 'click', onDocumentClick, false );
+		document.removeEventListener( 'touchstart', onDocumentClick, false );
+
+		container.classList.remove( 'avgrund-active' );
+		popup.classList.remove( 'avgrund-popup-animate' );
+
 	}
 
 	function disableBlur() {
-		document.documentElement.classList.add('no-blur');
+
+		document.documentElement.classList.add( 'no-blur' );
+
 	}
 
-	function show(selector){
-		popup = document.querySelector(selector);
-		popup.classList.add('avgrund-popup-animate');
+	function show( selector ) {
+
+		popup = document.querySelector( selector );
+		popup.classList.add( 'avgrund-popup-animate' );
 		activate();
 		return this;
+
 	}
+
 	function hide() {
+
 		deactivate();
+
 	}
 
 	return {
+
 		activate: activate,
 		deactivate: deactivate,
 		disableBlur: disableBlur,
 		show: show,
 		hide: hide
+
 	}
 
 })();
